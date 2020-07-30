483318e5923d835ef89b96e578dc177b513b47eb
solomon monotilla <solomon.monotilla@gmail.com>
2020-07-30 19:31:02 +0800

    finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect} from 'react';
-import { Text, View, TouchableWithoutFeedback, Button, StyleSheet, Dimensions, Alert} from 'react-native';
+import { Text, View, TouchableWithoutFeedback, StyleSheet, Dimensions, Alert} from 'react-native';
+import {Button} from 'react-native-paper';
 
 import { EventRegister } from 'react-native-event-listeners'
 
@@ -19,7 +20,8 @@ const TilePuzzle = () => {
   const [count, setCount] = useState(0);
   const [puzzleSequence, setpuzzleSequence] = useState([]);
   const [blankState, setBlankState] = useState({index:8,value:''});
-  const [moveDone, setmoveDone] = useState(0);
+  const [Refresh, setRefresh] = useState(0);
+  const [gotSolved, setgotSolved] = useState(0);
  
   const solveThis = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // 9 numbers to be solved.
 
@@ -46,11 +48,10 @@ isSolved++
 console.log('solved:' + isSolved);
 
 if (isSolved ==1 ){
-Alert.alert('solved!!');
+Alert.alert('Congratulations, you solved the puzzle!');
+setgotSolved(1);
 }
 
-
-  
 }
 
 function shuffle(arr) {
@@ -63,17 +64,42 @@ function shuffle(arr) {
   }
   return arr;
 };
+
 useEffect(() => {
-if (moveDone==0)
+if (Refresh==0)
 {  
-setpuzzleSequence(shuffle(solveThis));
-setmoveDone(1);
+  setpuzzleSequence(shuffle(solveThis));
+  setRefresh(1); // initialized
 }
 });
 
   return (
     <View style={styles.container}>
- 
+      <Button mode="text" style={styles.gameHeader} onPress={() => {
+        // moveTile(i, number);
+
+        Alert.alert(
+          'New Game',
+          'Would you like to leave current game and start new?',
+          [
+
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+              style: 'cancel'
+            },
+            { text: 'Yes', onPress: () => setpuzzleSequence(shuffle(solveThis)) }
+          ],
+          { cancelable: false }
+        );
+
+
+
+
+
+      }
+
+      }>Simple Tile Puzzle</Button>
       {puzzleSequence.map((number, i) => {
         if (i == blankState.index) {
           return <Text style={styles.puzzleBoxesHidden} index={i} onPress={() => moveTile(i, number)}></Text>
@@ -86,41 +112,87 @@ setmoveDone(1);
       }
 
       )}
+      <Button mode="contained" style={styles.newGameBtn} onPress={() => {
+        // moveTile(i, number);
+
+        Alert.alert(
+          'New Game',
+          'Would you like to leave current game and start new?',
+          [
+          
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+              style: 'cancel'
+            },
+            { text: 'Yes', onPress: () => setpuzzleSequence(shuffle(solveThis)) }
+          ],
+          { cancelable: false }
+        );
+
+
+
  
+
+      }
+
+      }>New Game</Button>
+    
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1, flexDirection: 'row', flexWrap: 'wrap', alignContent:'center'
+    flex: 1, 
+    flexDirection: 'row', 
+    flexWrap: 'wrap', 
+    alignContent: 'center', 
+    width: windowWidth-10, 
+    marginLeft:5, marginRight:5, marginTop:0, alignSelf:'center', alignItems:'center', borderColor:'red',
+    marginTop:-50
     
   },
   
   puzzleBoxes: {
-    width: windowWidth * .30, height: windowHeight * .30, backgroundColor: dynamicColor(), margin:5,
-    padding: 40,
+    width: windowWidth * .30, height: windowHeight * .20, backgroundColor: dynamicColor(), margin:5,
+    padding: 30,
     fontSize: 50,
  
     alignContent: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 0.50)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10
+    
 
   },
 
   puzzleBoxesHidden: {
-    width: windowWidth * .30, height: windowHeight * .30,   margin: 5,
+    width: windowWidth * .30, height: windowHeight * .20,   margin: 5,
     padding: 40,
     fontSize: 50,
+    backgroundColor:'grey',
 
     alignContent: 'center',
     textAlign: 'center'
 
   },
   
-  puzzleText: {
- 
-
-
+  newGameBtn: {
+    width: '100%',
+    marginTop:10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height:windowHeight*.10
   },
+  
+gameHeader:{   
+   width: '100%',
+    marginTop:-10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height:windowHeight*.10
+  }
   
 
 })
